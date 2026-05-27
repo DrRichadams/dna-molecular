971bfd58b1c7c29b38975ec9be1aec07c034ff3e
Richard <proxyserver7798@gmail.com>
2026-05-27 14:50:43 +0200

    Refactored the code

--- app/page.js
+++ app/page.js
@@ -2,10 +2,6 @@ import Banner from "@/comps/banner";
 import styles from "./page.module.css";
 import About from "@/comps/about";
 import Services from "@/comps/services";
-import HowItWorks from "@/comps/howitworks";
-import Why from "@/comps/whychooseus";
-import Contact from "@/comps/contact";
-import FAQ from "@/comps/faq";
 import Footer from "@/comps/footer";
 
 export default function Home() {
